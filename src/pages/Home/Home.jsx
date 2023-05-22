f99b07f202590bf53c2110180de3f433b2b15eee
RomaBortnik <bortnik25051998@gmail.com>
2023-05-22 19:34:17 +0300

    done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,34 @@
-import { HomePage } from './Home.styled';
+import {
+  HomePage,
+  HomePageContainer,
+  PhoneBook,
+  PhoneBookWrapper,
+  PhoneBookTitle,
+  PhoneBookBox,
+  PhoneBookDescr,
+} from './Home.styled';
+import phoneBook from '../../images/phoneBook.jpg';
+import phoneImage from '../../images/phoneImage.jpg';
 
 const Home = () => {
   return (
     <>
-      <HomePage></HomePage>
+      <HomePage>
+        <HomePageContainer>
+          <PhoneBookWrapper>
+            <PhoneBookBox>
+              <PhoneBookTitle>Phonebook</PhoneBookTitle>
+              <PhoneBookDescr>
+                The whole library is in your phone
+              </PhoneBookDescr>
+              <img src={phoneImage} alt="" />
+            </PhoneBookBox>
+          </PhoneBookWrapper>
+          <PhoneBookWrapper>
+            <PhoneBook src={phoneBook} alt="" />
+          </PhoneBookWrapper>
+        </HomePageContainer>
+      </HomePage>
     </>
   );
 };
